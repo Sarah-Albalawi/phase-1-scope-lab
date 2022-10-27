486032f4854b130fba6a007c09157b4061618b7d
Sarah-Albalawi <s.albalawi@outlook.com>
2022-10-27 17:29:06 +0300

    complete assignment

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Write your solution in this file!
+// Write your solution in this file!.
+
+var customerName = 'bob';
+var bestCustomer;
+const leastFavoriteCustomer = 'noor';
+
+function upperCaseCustomerName(){
+     customerName = 'BOB';
+    console.log(customerName);
+
+}
+ function setBestCustomer(){
+    bestCustomer = 'not bob';
+    console.log(bestCustomer);
+ 
+}
+
+
+function overwriteBestCustomer(){
+      bestCustomer ='maybe bob';
+     console.log(bestCustomer);
+
+}
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = 'sarah'
+}
